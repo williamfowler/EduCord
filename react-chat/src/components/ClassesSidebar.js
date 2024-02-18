a3f749bc84aac5d0e21d169dcac7c999206cab4c
wfowle01 <wfowle01>
2024-02-18 10:47:47 -0500

    merged Will w/ main

--- react-chat/src/components/ClassesSidebar.js
+++ react-chat/src/components/ClassesSidebar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 
-const ClassesSidebar = ({ classes, onSelectClass }) => {
+const ClassesSidebar = ({ /*classes, */ onSelectClass }) => {
+
+  const handleClassChange = (room) => {
+    onSelectClass(room); // Call callback function to change selected chat room
+  };
+
   return (
     <aside className="classes-sidebar">
       <h2>Classes</h2>
+      <button onClick={() => handleClassChange("Class 1")}> {/* Change the collection name */}
+        Class 1
+      </button>
+      <button onClick={() => handleClassChange("Class 2")}> {/* Change the collection name */}
+        Class 2
+      </button>
     </aside>
   );
 };
